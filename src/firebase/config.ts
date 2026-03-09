5eba115a7861e33e0299eb8e97abd34cd766f300
Apps Industry <appsindustry068@gmail.com>
2026-03-09 13:14:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2294305185-c0b44",
+  "appId": "1:1080412997001:web:6d84a90b1a5413d3124d58",
+  "apiKey": "AIzaSyBW4jwt8hVQ7uXU1Mg06Ppj4_zUYjBezXw",
+  "authDomain": "studio-2294305185-c0b44.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1080412997001"
+};
